a0a7bba57404e9b3a95cc1c21c655fb54fdbfc2b
J-Bailly <julien.bailly2@etu.univ-orleans.fr>
2024-11-05 15:51:42 +0100

    ajout constante et tableau

--- src/pages/creationGroupe.py
+++ src/pages/creationGroupe.py
@@ -7,4 +7,5 @@ class CreationGroupe(Page):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
         self.create_label("instruction_text", 0.5, 0.1, "Création du groupe", font=GRANDE_POLICE)
-        
+        tableau = Table(self, controller, 500,4,["Nom","Type","Minimal","Maximal"])
+        tableau.place(relx=0.5, rely=0.5, anchor='center', relwidth=0.5, relheight=0.5)
