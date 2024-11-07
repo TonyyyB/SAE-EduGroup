f6a7724b52a3de0bab957562e9f1889c9b90a100
Leo-Saunier <leo.saunier@etu.univ-orleans.fr>
2024-11-07 10:14:29 +0100

    modification classe Cat

--- src/modele/Cat.py
+++ src/modele/Cat.py
@@ -4,4 +4,5 @@ class Cat(TypeCritere.TypeCritere):
         
         def __init__(self, nom, poids):
             TypeCritere.TypeCritere.__init__(self, nom, poids)
+            self.transposition_valeur = dict()
         
