636c13d89daa16a6d4891391d1ad83731a3e36f4
MatheoLobjois <o22301531@or-iut-i202-f14.iut45.univ-orleans.fr>
2024-11-08 08:44:10 +0100

    Début d'algo

--- src/modele/algo.py
+++ src/modele/algo.py
@@ -1,7 +1,18 @@
-from modele.Critere import Critere
-from modele.Eleve import Eleve
-from modele.Groupe import Groupe
-from modele.Partition import Partition
+from modele.critere import Critere
+from modele.eleve import Eleve
+from modele.groupe import Groupe
+from modele.partition import Partition
 
-def algo():
+def algo(eleves,nb_groupes,tailles,contraintes):
+    i = 0
+    j = 0
+    # A revoir
+    for g in nb_groupes:
+        groupe = Groupe(tailles[g])
+    while i < len(eleves):
+        while j < nb_groupes:
+            #formule
+            j += 1
+        groupe.ajouterEleve(eleves[i])
+    
     pass
