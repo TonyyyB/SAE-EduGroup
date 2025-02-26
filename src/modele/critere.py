a5a77da02f9a1e39e68f98d1b585d6ac16bd15a2
Tony Beaujouan <tony.beaujouan2@gmail.com>
2025-02-26 14:59:02 +0100

    Début de la modification du modele

--- src/modele/critere.py
+++ src/modele/critere.py
@@ -1,70 +1,26 @@
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from groupe import Groupe
 class Critere:
-    def __init__(self, nom:str, poids:int, repartition:bool):
+    def __init__(self, nom:str, poids:int):
         self.nom:str = nom
         self.poids:int = poids
-        self.repartition:bool = repartition
-        self.transpo:dict[int|bool|str,int] = dict()
+        self.valeurs:set[str] = set()
 
-    def ajouter_valeur(self, valeur:int|bool|str, correspondance:int=None) -> None:
-        if valeur not in self.transpo.keys():
-            if correspondance is None:
-                if isinstance(valeur, int):
-                    correspondance = valeur
-                elif isinstance(valeur, bool):
-                    correspondance = 1 if valeur else 0
-                else:
-                    if len(valeur) == 1:
-                        correspondance = ord(valeur) - ord('A') + 1
-                    else:
-                        correspondance = len(self.transpo.keys())+1
-            self.transpo[valeur] = correspondance
+    def ajouter_valeur(self, valeur:str) -> None:
+        self.valeurs.add(valeur)
 
     def set_poids(self, poids:int) -> None:
         self.poids:int = poids
+
     def get_poids(self) -> int:
         return self.poids
-    
-    def set_repratis(self, repartis:bool) -> None:
-        self.repartition:bool = repartis
 
     def get_nom(self) -> str:
         return self.nom
-    def est_reparti(self) -> bool:
-        return self.repartition
-
-    def to_int(self, val:int|bool|str) -> int:
-        return self.transpo[val]
-
-    def to_val(self, val:int) -> int|bool|str:
-        for cle, valeur in self.transpo.items():
-            if valeur == val:
-                return cle
-        return None
-
-    def calcul_score(self, groupe:'Groupe') -> float:
-        pass
-
-    def get_transpo(self) -> dict[int|bool|str,int]:
-        return self.transpo
     
-    def get_valeurs_possibles(self, toVal=False) -> set[int]|set[int|bool|str]:
-        return set(self.transpo.keys()) if toVal else set(self.transpo.values())
-    
-    def get_valeur_min(self, toVal=False) -> int|bool|str:
-        if(toVal):
-            return min(self.transpo, key = self.transpo.get)
-        return min(self.transpo)
-    
-    def get_valeur_max(self, toVal=False) -> int|bool|str:
-        if(toVal):
-            return max(self.transpo, key = self.transpo.get)
-        return max(self.transpo)
+    def get_valeurs(self) -> set[str]:
+        return self.valeurs
 
     def __repr__(self):
         return f"{self.nom}: {self.poids}"
 
     def __hash__(self) -> int:
-        return hash(self.nom)
+        return hash(self.nom, self.poids)
--- src/modele/groupe.py
+++ src/modele/groupe.py
@@ -1,12 +1,9 @@
 from modele.eleve import Eleve
 from modele.critere import Critere
 class Groupe:
-    def __init__(self, taille:int, contraintes:set[Critere]=None):
+    def __init__(self, taille:int, contraintes:set[Critere]=dict()):
         self.taille:int = taille
-        if contraintes is None:
-            self.contraintes:dict[Critere,set[int]] = dict()
-        else:
-            self.contraintes:dict[Critere,set[int]] = contraintes  # Dictionnaire pour les contraintes
+        self.contraintes:dict[Critere,set[str]] = contraintes  # Dictionnaire pour les contraintes
         self.eleves:set[Eleve] = set()
         self.criteres:set[Critere] = set()
         self.aEteModifier = False
@@ -16,6 +13,9 @@ class Groupe:
             self.aEteModifier = True
         self.taille = taille
     
+    def set_a_ete_modifier(self, aEteModifier) -> None:
+        self.aEteModifier = aEteModifier
+    
     def a_ete_modifier(self) -> bool:
         return self.aEteModifier
     
@@ -23,11 +23,11 @@ class Groupe:
         self.contraintes[critere] = set(vals)
 
     def respecter_contraintes(self, eleve:Eleve):
-        """Vérifie si un élève respecte les contraintes d'un groupe."""
         for critere, valeurs in self.contraintes.items():
             if eleve.get_critere(critere) not in valeurs:
                 return False
         return True
+    
     def ajouter_eleve(self, eleve:Eleve) -> None:
         if len(self.eleves) + 1 > self.taille:
             return False
@@ -41,50 +41,6 @@ class Groupe:
 
     def place_dispo(self) -> bool:
         return len(self.eleves) + 1 <= self.taille
-
-    def simule_ajout(self, eleve:Eleve) -> float:
-        if eleve in self.eleves or len(self.eleves) + 1 > self.taille: return self.calcul_score()
-        self.eleves.add(eleve)
-        score = self.calcul_score()
-        self.eleves.remove(eleve)
-        return score
-
-    def simule_supp(self, eleve:Eleve) -> float:
-        if eleve not in self.eleves: return self.calcul_score()
-        self.eleves.remove(eleve)
-        score = self.calcul_score()
-        self.eleves.add(eleve)
-        return score
-
-    def simule_transf(self, groupe:'Groupe', eleve1:Eleve, eleve2:Eleve) -> tuple[float,float]: # type: ignore
-        """Renvoie les deux score des deux groupes si un transfer est effectuer entre les deux élèves
-
-        Args:
-            groupe (Groupe): groupe concerné
-            eleve1 (Eleve): eleve du groupe
-            eleve2 (Eleve): eleve de l'autre groupe
-
-        Returns:
-            (float,float): score du groupe, score de l'autre groupe
-        """
-        if eleve1 not in self.eleves or eleve2 not in groupe.get_eleves(): return self.calcul_score(), groupe.calcul_score()
-        self.eleves.remove(eleve1)
-        groupe.get_eleves().remove(eleve2)
-        self.eleves.add(eleve2)
-        groupe.get_eleves().add(eleve1)
-        scores = self.calcul_score(), groupe.calcul_score()
-        self.eleves.remove(eleve2)
-        groupe.get_eleves().remove(eleve1)
-        self.eleves.add(eleve1)
-        groupe.get_eleves().add(eleve2)
-        return scores
-
-    def transferer(self, groupe:'Groupe', eleve1:Eleve, eleve2:Eleve):
-        if eleve1 not in self.eleves or eleve2 not in groupe.get_eleves(): return self.calcul_score(), groupe.calcul_score()
-        self.eleves.remove(eleve1)
-        groupe.get_eleves().remove(eleve2)
-        self.eleves.add(eleve2)
-        groupe.get_eleves().add(eleve1)
 
     def get_eleves(self) -> set[Eleve]:
         return self.eleves
